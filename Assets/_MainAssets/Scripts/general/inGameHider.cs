855635609514f56c634982a90ddd760720d7326a
jmickles <104017719+theJAMARK1@users.noreply.github.com>
2024-11-14 10:46:56 -0600

    Simple street scene

---
+++ Assets/_MainAssets/Scripts/general/inGameHider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inGameHider : MonoBehaviour
+{
+    Renderer test;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Makes the object INVISIBLE in game mode (if FALSE)
+        test = GetComponent<MeshRenderer>();
+        test.enabled = false;
+    }
+
+    //Update called once per frame
+    void Update()
+    {
+
+    }
+}

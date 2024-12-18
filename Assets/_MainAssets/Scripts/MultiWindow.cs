08160dce56d18d662df5e570a0b50347e1292acc
James L. <lang.n.james@gmail.com>
2024-12-18 02:03:48 -0600

    MultiWindow Work

---
+++ Assets/_MainAssets/Scripts/MultiWindow.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiWindow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Activate all additional displays (if available)
+        for (int i = 1; i < Display.displays.Length; i++)
+        {
+            Display.displays[i].Activate();
+        }
+    }
+}
